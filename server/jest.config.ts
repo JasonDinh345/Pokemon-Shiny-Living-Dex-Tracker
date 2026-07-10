bf6e035cdabb7cb2a7c96c23d3518e5069d6492c
Jason Dinh <jasondinh345@gmail.com>
2026-07-10 16:37:38 -0700

    setup test to use a test db

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -7,4 +7,5 @@ export default {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ["ts", "js", "json", "node"],
+  setupFiles: ["<rootDir>/src/tests/setup.ts"],
 };
